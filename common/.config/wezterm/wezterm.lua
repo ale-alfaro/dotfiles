48c473d28a2cd5483e91285a24bc65ca0db67724
Ale Alfaro <alejandro.alfaro@sibelhealth.com>
2025-09-14 15:16:09 -0700

    changed the modifier to swithc panes

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 local colors = require("utils.colors")
 colors.init()
@@ -21,5 +22,16 @@ else
 	error("Unsupported target triple: " .. wezterm.target_triple)
 end
 require("config.events").setup()
+
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	-- modifier keys to combine with direction_keys
+	modifiers = {
+		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "CMD", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+	-- log level to use: info, warn, error
+	log_level = "info",
+})
 
 return config
