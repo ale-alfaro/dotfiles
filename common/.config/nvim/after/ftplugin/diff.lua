5d9fb69e04eac06d502dbabb53584aebf7a9576f
Ale Alfaro <ale16alfaro@gmail.com>
2026-04-27 15:19:07 -0700

    [2026-04-27]

---
+++ common/.config/nvim/after/ftplugin/diff.lua
@@ -0,0 +1,2 @@
+vim.cmd [[ setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() ]]
+vim.keymap.set({ 'n', 'x' }, '<Leader>gs', '<Cmd>lua MiniGit.show_at_cursor()<CR>', { desc = 'Show at cursor' })
